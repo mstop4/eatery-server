a4af46d353bc08f97bbed78f4dc51a35bb5959a3
mstop4 <mstop4@gmail.com>
2017-08-15 18:07:31 -0400

    merge attempt

--- routes/places.js
+++ routes/places.js
@@ -7,11 +7,13 @@ router.get('/', function(req, res, next) {
 
   var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=43.644359,-79.3951126&type=restaurant&radius=200&key=AIzaSyAlibWfdCwp1PWyXZtVVNlVd_BfU39Oj8o"
 
+  console.dir(req)
+
   request(url, function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    res.setHeader('Content-Type', 'application/json');
-    res.send(body);
+    response.setHeader('Content-Type', 'application/json');
+    response.send(body);
     });
 });
 
