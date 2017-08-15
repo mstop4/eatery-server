537e8aacd264447324a04345050caa3e08290bbf
mstop4 <mstop4@gmail.com>
2017-08-15 17:12:31 -0400

    minor changes

--- routes/places.js
+++ routes/places.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET map json. */
 router.get('/', function(req, res, next) {
 
-  var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=43.7993947,-79.1581265&type=restaurant&radius=1000&key=AIzaSyAlibWfdCwp1PWyXZtVVNlVd_BfU39Oj8o"
+  var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=43.644359,-79.3951126&type=restaurant&radius=200&key=AIzaSyAlibWfdCwp1PWyXZtVVNlVd_BfU39Oj8o"
 
   request(url, function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
