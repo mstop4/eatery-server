a576833f78cb81141eac13f9c47beac83ce63a48
Arnold Chan <arnoldthchan@gmail.com>
2017-08-17 14:57:35 -0400

    Added routing for login and logout(sessions)

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const session = require('express-session');
+
+router.use(session({secret: 'moist'}));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', function(req, res, next) {
-  console.log(req.body);
+var sess;
+
+router.post('/facebook', function(req, res, next) {
+  fb = req.body
+  console.log(fb);
+  sess=req.session;
+  sess.cookie.email=fb.email;
+  sess.cookie.name=fb.name;
+  sess.cookie.fbId=fb.fbId;
+  console.log('SESSION INFO:' ,sess )
   //BLAH BLAH BLAH REGISTER USER IN DB HERE
 });
 
+router.get('/logout', function(req, res, next){
+  sess = null;
+  console.log('LOGOUT')
+});
 module.exports = router;
