c2e8182755d8e9d41bfc6b20955c6528daa9fe1c
mstop4 <mstop4@gmail.com>
2017-08-18 15:33:06 +0000

    can search for any establishment type

--- routes/places.js
+++ routes/places.js
@@ -3,10 +3,10 @@ var request = require("request");
 var router = express.Router();
 
 /* GET map json. */
-// Queries require: lat, lng, radius
+// Queries require: lat, lng, type, rankby
 router.get('/', function(routerReq, routerRes, next) {
 
-    var url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${routerReq.query.lat},${routerReq.query.lng}&type=restaurant&radius=${routerReq.query.radius}&key=${process.env.GOOGLEMAPS_APIKEY}`;
+    var url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${routerReq.query.lat},${routerReq.query.lng}&type=${routerReq.query.type}&rankby=${routerReq.query.rankby}&key=${process.env.GOOGLEMAPS_APIKEY}`;
 
     request(url, function (error, requestRes, body) {
       console.log('error:', error); // Print the error if one occurred
